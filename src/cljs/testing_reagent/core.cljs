d91b2b8fc49032574cc671b594d6693e60a315c8
Jack Mocherman <mochermanjack@gmail.com>
2016-11-03 12:49:13 -0500

    changed gitignore to ignore compiled js files, added button that console logs hey on click

--- src/cljs/testing_reagent/core.cljs
+++ src/cljs/testing_reagent/core.cljs
@@ -3,11 +3,14 @@
    [reagent.core :as reagent :refer [atom]]
    ))
 
+(enable-console-print!)
+
 (defonce app-state (atom {:text "Hello world"}))
 
 (defn hello-world []
   [:div
-    [:h1 (:text @app-state)]])
+    [:h1 (:text @app-state)]
+    [:button {:on-click #(println "hey")} "Push Me"]])
 
 (reagent/render-component [hello-world]
   (. js/document (getElementById "app")))
